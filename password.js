ed99760248053dec35edf16830d0ce3189ce3858
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-17 23:20:45 -0500

    final commit

--- password.js
+++ password.js
@@ -1,20 +1,18 @@
 // Create a new file called password.js. Write a program that does the following:
 // // Welcome the user to the password validator tool
 // // Prompt the user for a password to validate
-// // Check if the user’s password meets the following constraint:
-// // At least 10 characters long
+// // Check if the user’s password meets the following constraint: At least 10 characters long
 // // If the user’s password meets the constraint, show a success message to the user
 // // If the user’s password fails the constraint, show a failure message to the user
 
-let welcomMsg = "Welcome to the password validator tool!"
+const welcomeMsg = ("Welcome user!")
+console.log(welcomeMsg)
 
-let password = "Password10"
+let password = "Password123"
 
-for (let i = 0; i >= 10; i++)
 
-if (password >= 10){
+if (password.length >= 10){
     console.log("Success!")
-}else{
-    console.log("Password too short!")
-}
+}else{ console.log("Failure")
 
+}
